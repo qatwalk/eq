bf3f5e76d4b79591b493420802b4bdc8eec60da9
qablet <somdip@alumni.princeton.edu>
2024-09-03 08:54:21 -0400

    consolidate LV calibration in class SVItoLV

--- src/model/localvol.py
+++ src/model/localvol.py
@@ -12,7 +12,7 @@ from numpy.random import SFC64, Generator
 
 
 def svi(params, k):
-    """Get total variance from SVI params and log strikes."""
+    """Get total variance (v^2 t) from SVI params and log strikes."""
     k_m = k - params["m"]
     discr = np.sqrt(k_m**2 + params["sig"] ** 2)
     w = params["a"] + params["b"] * (params["rho"] * k_m + discr)
@@ -20,18 +20,28 @@ def svi(params, k):
 
 
 class SVItoLV:
-    def __init__(self, k_vec, svi_df):
-        """Create a 2-D array of total variances (ws) by t and k."""
+    """Helper class to convert SVI parameters to local volatility."""
+
+    def __init__(self, svi_df):
+        """Create a 2-D array of total variances (ws) by t and k.
+        The knots in t are the same as in the input svi_df."""
+        kmin, kmax, dk = -5.0, 5.0, 0.025
+        self.dk = dk
+        self.k_vec = np.arange(kmin - dk, kmax + dk + dk / 2, dk)
+        self.k_vec_prime = self.k_vec[1:-1]
+
         self.t_vec = svi_df["texp"]
-        self.k_vec = k_vec
         self.t_len = len(self.t_vec)
 
-        self.ws = np.zeros((len(self.t_vec), len(k_vec)))
+        self.ws = np.zeros((len(self.t_vec), len(self.k_vec)))
         for i, t in enumerate(self.t_vec):
-            self.ws[i] = svi(svi_df.loc[i], k_vec)
+            self.ws[i] = svi(svi_df.loc[i], self.k_vec)
+
+        self.w_vec_prev = np.zeros(len(self.k_vec))
 
     def total_var(self, t):
-        """Return the total variance vector at time t."""
+        """Return the total variance (v^2 t) vector at time t
+        by interpolating from self.ws."""
 
         i_left = np.searchsorted(self.t_vec, t, side="left")
         i_right = np.searchsorted(self.t_vec, t, side="right")
@@ -54,32 +64,44 @@ class SVItoLV:
             w = self.ws[i_left]
         return w
 
+    def local_vol(self, prev_time, new_time):
+        """Advance w_vec to new_time and return the local volatility vector between prev_time and new time."""
+        w_vec = self.total_var(new_time)
+        lvar = self._svi_local_var_step(
+            prev_time,
+            new_time,
+            self.w_vec_prev,
+            w_vec,
+        )
 
-def _svi_local_var_step(k, dk, t0, t1, w_vec_prev, w_vec):
-    """Calculate local variance (vol**2) from t0 to t1, given a list of strikes k, uniformly spaced by dk,
-    and the total variances (w = T * vol**2) at time t0 and t1.
-    The result vector is two elements shorter than the input vectors k and w."""
+        self.w_vec_prev = w_vec
+        return np.sqrt(lvar)
 
-    w = (w_vec + w_vec_prev) / 2
-    # time derivative of w
-    wt = (w_vec - w_vec_prev) / (t1 - t0)
+    def _svi_local_var_step(self, t0, t1, w_vec_prev, w_vec):
+        """Calculate local variance (vol**2) from t0 to t1, given a list of strikes k, uniformly spaced by dk,
+        and the total variances (w = T * vol**2) at time t0 and t1.
+        The result vector is two elements shorter than the input vectors k and w."""
 
-    # strike derivatives of w
-    wk = (w[2:] - w[:-2]) / (2 * dk)
-    wkk = (w[2:] + w[:-2] - 2 * w[1:-1]) / (dk**2)
+        w = (w_vec + w_vec_prev) / 2
+        # time derivative of w
+        wt = (w_vec - w_vec_prev) / (t1 - t0)
 
-    # drop the extra points at top and bottom
-    w_ = w[1:-1]
-    k_ = k[1:-1]
-    wt_ = wt[1:-1]
+        # strike derivatives of w
+        wk = (w[2:] - w[:-2]) / (2 * self.dk)
+        wkk = (w[2:] + w[:-2] - 2 * w[1:-1]) / (self.dk**2)
 
-    # apply Dupire's formula
-    return wt_ / (
-        1
-        - k_ / w_ * wk
-        + 1 / 4 * (-1 / 4 - 1 / w_ + k_**2 / w_**2) * (wk) ** 2
-        + 1 / 2 * wkk
-    )
+        # drop the extra points at top and bottom
+        w_ = w[1:-1]
+        k_ = self.k_vec[1:-1]
+        wt_ = wt[1:-1]
+
+        # apply Dupire's formula
+        return wt_ / (
+            1
+            - k_ / w_ * wk
+            + 1 / 4 * (-1 / 4 - 1 / w_ + k_**2 / w_**2) * (wk) ** 2
+            + 1 / 2 * wkk
+        )
 
 
 # Define a class for the state of a single asset BS Local Vol MC process
@@ -96,14 +118,9 @@ class LVMC(MCFixedStep):
             self.dataset["ASSETS"][self.dataset["BASE"]]
         )
 
-        # initialize states related to local vol calibration
-        svi_df = self.dataset["LV"]["VOL"]
-        kmin, kmax, dk = -5.0, 5.0, 0.025
-        self.dk = dk
-        self.k_vec = np.arange(kmin - dk, kmax + dk + dk / 2, dk)
-        self.svi_lv = SVItoLV(self.k_vec, svi_df)
+        # initialize helper for local vol calibration
+        self.svi_lv = SVItoLV(self.dataset["LV"]["VOL"])
         self.logspot = np.log(self.spot)
-        self.w_vec_prev = np.zeros(len(self.k_vec))
 
         # Initialize rng and any arrays
         self.rng = Generator(SFC64(self.dataset["MC"]["SEED"]))
@@ -113,32 +130,17 @@ class LVMC(MCFixedStep):
 
         self.cur_time = 0
 
-    def _advance_vol(self, prev_time, new_time):
-        """Advance the local realized var by time dt and return the vol at the new time."""
-        fwd = self.asset_fwd.forward(prev_time)
-        logfwd_shift = np.log(fwd) - self.logspot
-
-        w_vec = self.svi_lv.total_var(new_time)
-        lvar = _svi_local_var_step(
-            self.k_vec,
-            self.dk,
-            prev_time,
-            new_time,
-            self.w_vec_prev,
-            w_vec,
-        )
-        vol_by_k = np.sqrt(lvar)
-
-        self.w_vec_prev = w_vec
-        return np.interp(self.x_vec - logfwd_shift, self.k_vec[1:-1], vol_by_k)
-
     def step(self, new_time):
         """Update x_vec in place when we move simulation by time dt."""
 
         dt = new_time - self.cur_time
-
         fwd_rate = self.asset_fwd.rate(new_time, self.cur_time)
-        vol = self._advance_vol(self.cur_time, new_time)
+
+        # Get local vol, from current time to new time)
+        logfwd_shift = self.asset_fwd.log_forward_fn(new_time) - self.logspot
+        np.subtract(self.x_vec, logfwd_shift, out=self.tmp)
+        vol_by_k = self.svi_lv.local_vol(self.cur_time, new_time)
+        vol = np.interp(self.tmp, self.svi_lv.k_vec_prime, vol_by_k)
 
         # # generate the random numbers and advance the log stock process
         self.rng.standard_normal(self.n, out=self.dz_vec)
--- src/model/localvol_profile.py
+++ src/model/localvol_profile.py
@@ -8,7 +8,7 @@ from localvol_example import run_model  # noqa: F401
 
 if __name__ == "__main__":
     # Create a LineProfiler object, specifying the methods to be profiled by line
-    lprofiler = LineProfiler(LVMC.step, LVMC._advance_vol)
+    lprofiler = LineProfiler(LVMC.step)
     # Wrap the entry function with the LineProfiler object, then run it.
     lp_wrapper = lprofiler(run_model)
     lp_wrapper()
